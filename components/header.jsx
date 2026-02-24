28c76598fa7534ca3868145f61f46322052f3865
surya599 <suryabhuvaneshwarreddy@gmail.com>
2026-02-24 23:58:40 +0530

    done header part of the project

---
+++ components/header.jsx
@@ -0,0 +1,9 @@
+export default function Header(){
+    return(
+        <header className="header-outer">
+                <img src="../cheficon2.png" alt="chef icon" />
+                <span>Chef Claude</span>
+        </header>
+    )
+
+}
