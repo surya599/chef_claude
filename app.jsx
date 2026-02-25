acdca569547fc89fa3d492e5c5ad0b45dabfeb44
surya599 <suryabhuvaneshwarreddy@gmail.com>
2026-02-26 00:33:06 +0530

    done with form element

--- app.jsx
+++ app.jsx
@@ -1,9 +1,13 @@
 import Header from "./components/header";
+import Main from "./components/main";
 
 export default function App()
 {
     return(
+        <>
         <Header/>
+        <Main/>
+        </>
     )
     
 }
---
+++ components/main.jsx
@@ -0,0 +1,11 @@
+export default function Main()
+{
+    return(
+        <main>
+            <form className="add-ingredients-form">
+        <input type="text" placeholder="Eg. Oregano"  aria-label="Add ingredient"/>
+        <button>+ Add Ingredient</button>
+        </form>
+        </main>
+    )
+}
