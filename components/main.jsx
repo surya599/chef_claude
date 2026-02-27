6e3f4910060dfeb649bb0bf3033995642c9a43c4
surya599 <suryabhuvaneshwarreddy@gmail.com>
2026-02-27 23:10:35 +0530

    added few ingredients to the page manually

--- components/main.jsx
+++ components/main.jsx
@@ -1,11 +1,33 @@
 export default function Main()
 {
+    const ingredients = ["chicken","oregano","butter"]
+    const ingredientslistitems = ingredients.map((ingredient) =>{
+        return(
+            <li key = {ingredient}>{ingredient}</li>
+        )
+    })
+
+    function handlesubmit(event){
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        ingredients.push(newIngredient)
+        console.log(ingredients)
+    }
     return(
         <main>
-            <form className="add-ingredients-form">
-        <input type="text" placeholder="Eg. Oregano"  aria-label="Add ingredient"/>
+            <form onSubmit={handlesubmit} className="add-ingredients-form">
+
+        <input type="text" placeholder="Eg. Oregano"  aria-label="Add ingredient" name="ingredient"/>
         <button>+ Add Ingredient</button>
+
         </form>
+        <div className="ingredients-list">
+        <ul>
+            {ingredientslistitems}
+        </ul>
+            </div>
         </main>
+        
     )
 }
